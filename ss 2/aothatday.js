3218f6076defec800fcb550381d9e11f07324229
manhqu <quan849628@gmail.com>
2023-07-09 11:21:15 +0700

    code cp2

---
+++ ss 2/aothatday.js
@@ -0,0 +1,61 @@
+// let i = 0
+// while(i<=10){
+//     console.log(i);
+//     i = i+1;
+// }
+
+
+
+// do {
+    //     console.log(i);
+    //     i++;
+    // } while (i<=10);
+    
+    
+// let sum = 0
+// for(let i =1;i<=10;i++){
+//     sum = sum + i;
+// }
+// console.log(sum);
+    // let Arr;
+    // Arr = [4,5,8,7]
+    // console.log(Arr.lengh);
+
+    // Arr.push(12,144);
+    // console.log(Arr);
+    // Arr.unshift(0)
+
+    // Arr.shift();
+    // console.log(Arr);
+    // Arr.pop();
+    // console.log(arr);
+
+    // let arr = [1, 2, 3, 4]
+    // arr.splice(1,1);
+    // console.log(arr);
+
+
+
+
+
+    let menu = [ "cơm rang" , "rau xào" , "cơm chiên"]
+    let vitriMonRauXao = menu.indexOf("rau xào")
+    console.log("vitriMonRauXao:", vitriMonRauXao);
+
+    menu[vitriMonRauXao] = "Rau mồng tơi"
+
+    console.log(menu);
+
+
+
+    let input = prompt("Mời Người Dùng Nhập Vào C,R,U,D")
+    console.log("input:", input);
+
+
+
+
+
+
+
+
+
